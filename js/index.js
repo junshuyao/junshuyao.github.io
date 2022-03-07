6a2d5e0994e408ab1324b42e120b630a2fd839f9
junshuyao <1853072347@qq.com>
2022-03-07 22:54:06 +0800

    test

--- js/index.js
+++ js/index.js
@@ -113,7 +113,7 @@ let counter = 0;
 c2.width = c3.width = window.innerWidth;
 c2.height = c3.height = window.innerHeight;
 ctx1.fillStyle = '#000';
-const text = '王瑞成';
+const text = 'WRC';
 let textWidth = 99999999;
 
 while (textWidth > window.innerWidth) {
