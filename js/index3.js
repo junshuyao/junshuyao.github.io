c932ce4f84986097a4205d34a2c15960ffafc89e
junshuyao <1853072347@qq.com>
2022-03-08 21:54:10 +0800

    nihaoya

--- js/index3.js
+++ js/index3.js
@@ -113,7 +113,7 @@ let counter = 0;
 c2.width = c3.width = window.innerWidth;
 c2.height = c3.height = window.innerHeight;
 ctx1.fillStyle = '#000';
-const text = 'WRC你好呀！';
+const text = 'LBK你好呀！';
 let textWidth = 99999999;
 
 while (textWidth > window.innerWidth) {
